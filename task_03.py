89cc36fbdae00ce22113dd9d8236848e74e9eca8
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-10-10 05:59:30 +0000

    need help on decimal

--- task_03.py
+++ task_03.py
@@ -3,14 +3,39 @@
 """For-looping a Simple data"""
 
 
-import data
 import decimal
-to_analyze = ('''Don't stop believing, Hold on to that feeling.''')
 
 def lexicographics(to_analyze):
-    to_analyze_split_comma = to_analyze.split(', ')
-    split_space = to_analyze.split (' ') 
-    myind = 0
-    for item in to_analyze_split_comma:
-        myind += 1
-        print myind, item
+    """This is a for loop for a simple data.
+
+    This function provides the maxium, minumum, and average length of words
+    in a speech performing a lexicograshical analysis.
+
+    Args:
+        to_analyze(mixed): provides texts.
+
+    Returns:
+        The maximum and minimum number of words, and average number of words
+        per line in to_analyze.
+
+    Examples:
+        >>> import task_03
+        >>> task_03.lexicographics('''Don't stop believing,
+        Hold on to that feeling.''')
+        (5, 3, Decimal(4.0))
+
+        >>> import task_03
+        >>> import data
+        >>> task_03.lexicographics(data.SHAKESPEARE)
+        (12, 5, Decimal('8.14'))
+
+    """
+    split_comma = to_analyze.split('\n')
+    length_list = []
+    for item in split_comma:
+        line_length = len(item.split(' '))
+        length_list.append(line_length)
+    max_num = max(length_list)
+    min_num = min(length_list)
+    avg_num = (decimal.Decimal(max_num) + min_num) / 2
+    return max_num, min_num, avg_num
